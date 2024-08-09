d15ae5257fe124d6f7fab39e1ca0acabfe511057
Easwaran <easwaranpottiak@gmail.com>
2024-08-09 20:54:30 +0530

    final commit

---
+++ 30 Create Hello World Function.js
@@ -0,0 +1,27 @@
+// Write a function createHelloWorld. It should return a new function that always returns "Hello World".
+ 
+
+// Example 1:
+
+// Input: args = []
+// Output: "Hello World"
+// Explanation:
+// const f = createHelloWorld();
+// f(); // "Hello World"
+
+// The function returned by createHelloWorld should always return "Hello World".
+
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    
+    return function(...args) {
+        console.log("Hello World")
+    }
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
